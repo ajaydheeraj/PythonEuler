6b9ffda7790362808371902adf8e0002ed9190c2
obliviator1 <obliviator2718@gmail.com>
2014-02-06 18:34:36 -0500

    Update euler1.py

--- Euler1-10/euler1.py
+++ Euler1-10/euler1.py
@@ -1,7 +1,10 @@
+# Euler 1: Find the sum of all the multiples of 3 or 5 below 1000.
 count=0
 for i in range(1000):
-    if (i%3==0) or (i%5==0):
-        count+=i
-print count
-
+    if (i%3==0) or (i%5==0): # if i divides into 3 evenly or 5 evenly
+        count+=i #add it to the count
+print count #print the count
+#print the sum of i in range(1000) if i % 3 is false(0) or i % 5
 print sum(i for i in range(1000) if not i % 3 or not i % 5)
+
+
