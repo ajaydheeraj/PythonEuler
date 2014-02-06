a79e457e9f1a1729ca18c1b5f00a04932a66c9f9
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-06 18:48:39 -0500

    def sum_under_term(x):
        a,b,s = 1,1, 0
        while b < x:
            if b % 2 == 0:
                s += b
            a,b = b, a+b
        print s
    
    sum_under_term(4000000)

--- Euler1-10/euler2.py
+++ Euler1-10/euler2.py
@@ -8,14 +8,3 @@ def sum_under_term(x):
     print s
 
 sum_under_term(4000000)
-
-def term_for_digit_length(y):
-    a,b,x = 1,1,2
-    while len(str(b)) < y:
-        a,b = b, a+b
-        x += 1
-    else:
-        print x 
-          
-term_for_digit_length(1000)
-        
