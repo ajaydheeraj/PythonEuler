175561b4c27ea381fde88ad207bae0177a749c09
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-08 21:26:31 -0500

    fixed duplication

--- Euler1-10/euler10.py
+++ Euler1-10/euler10.py
@@ -1,10 +1,3 @@
-def sieve(n): # all primes under number
-    l = [True] * (n+1) # set up grid
-    l[0] = l[1] = False# mark 0 and 1 as not prime
-    for i in xrange(2,int(n**0.5)):# loop up to the sqrt of n
-        if l[i]:#if l[i] is true
-            for a in xrange(i*i,n+1,i):#all remaining increments of i
-                l[a] = False#mark them all false
-    return [i for i,j in enumerate(l) if j == True]# return the values of the prime numbers
+from primesieve import * #well documented code
 
-print sum(sieve(2000000))
+print sum(sieve(2000000)) #sum of all primes below 2000000
