02b6348a4b1171979a0745c72ed01ae03169a93b
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-13 22:12:36 -0500

    much better

--- Euler40-50/euler48.py
+++ Euler40-50/euler48.py
@@ -1,12 +1,1 @@
-'''
-count = 0
-for i in range(1,1001):
-    count += i ** i
-print str(count)[-10:]
-Explanation for code:
-adding the number ** number to the count
-converting it to a string
-getting the digits from the 10th from the right, to the end
-'''
-print str(sum(i ** i for i in range(1,1001)))[-10:]
-
+print sum(i**i for i in range(1,1001)) % 10 ** 10
