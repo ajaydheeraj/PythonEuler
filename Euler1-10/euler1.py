ac30cee8979373a12e64dc39b92d3438f7101f8b
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-17 23:38:16 -0500

    fun

--- Euler1-10/euler1.py
+++ Euler1-10/euler1.py
@@ -7,4 +7,4 @@ print count #print the count
 #print the sum of i in range(1000) if i % 3 is false(0) or i % 5
 print sum(i for i in range(1000) if not i % 3 or not i % 5)
 
-
+print sum(set(range(0,1000,3) + range(0,1000,5)))
