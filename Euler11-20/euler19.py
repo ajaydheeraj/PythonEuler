c07c8090da7067abf80618f494955d50d184e0d6
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-23 11:29:00 -0500

    edits

--- Euler11-20/euler19.py
+++ Euler11-20/euler19.py
@@ -1,21 +1,4 @@
 import calendar
-'''
-count = 0
 
-month= 1
-year = 1901
-while year < 2001:
-    if calendar.weekday(year,month,1) == 6:
-        count +=1
-    month +=1
-    if month == 13:
-        year += 1
-        month = 1
-print count
-'''
-count = 0
-for year in range(1901,2001):
-    for month in range(1,13):
-        if calendar.weekday(year,month,1) == 6:
-            count +=1
-print count
+# one liner woot
+print sum(1 for y in range(1901,2001) for m in range(1,13) if calendar.weekday(y,m,1) == 6)
