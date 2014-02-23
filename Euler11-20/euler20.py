8293b8af6aa9608767d354768aba8e10707830ee
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-23 11:30:28 -0500

    made my own function

--- Euler11-20/euler20.py
+++ Euler11-20/euler20.py
@@ -1,3 +1,9 @@
 #sum of digits in 100!
 from math import factorial
 print sum([int(x) for x in str(factorial(100))])
+
+#if you want me to make my own function...
+
+def factorial1(n):
+    return n * factorial(n-1)
+print sum(int(x) for x in str(factorial1(100)))
