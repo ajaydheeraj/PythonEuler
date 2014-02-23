8331fccc5f9077e92d8375c09ed1e31d3a2db6c6
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-23 10:46:31 -0500

    much better

--- Euler1-10/euler3.py
+++ Euler1-10/euler3.py
@@ -1,10 +1,8 @@
 from primesieve import *
 # The above code is excessively documented
-
-p = sieve(int(600851475143**0.5)) #sieve up to the square root of number
-l = [] #empty list
 n = 600851475143
+p = sieve(int(n**0.5)) #sieve up to the square root of number
 for i in p[::-1]:
-    if n % i == 0: # if a number in the sieve(the largest) and the giant number divide evenly
+    if not n % i: # if a number in the sieve(the largest) and the giant number divide evenly
         print i #print the number
         break   #we're done here
