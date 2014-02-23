0a6df8c88121b9b623b06681052190bc7785639f
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-23 11:34:31 -0500

    shorter code

--- Euler21-30/euler21.py
+++ Euler21-30/euler21.py
@@ -2,15 +2,12 @@ def sdivisors(x):
     y = set(reduce(list.__add__, ([i,x//i] for i in range(1,int(x**0.5) + 1) if not x % i)))
     y.remove(x)
     return sum(y)
-
+'''
 a, c = 2,[]
 while a < 10000:
     if sdivisors(sdivisors(a)) == a and a != sdivisors(a):
         c.append(a)
-        c.append(sdivisors(a))
     a += 1
-print sum(set(c))
-
-
-
-    
+print sum(c)
+'''
+print sum(i for i in range(2,10000) if sdivisors(sdivisors(i)) == i and i != sdivisors(i))
