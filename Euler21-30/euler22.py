fb82637832191ecf30d00180b469e62713ac340b
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-23 22:57:46 -0500

    commented,shorter

--- Euler21-30/euler22.py
+++ Euler21-30/euler22.py
@@ -2,17 +2,8 @@ x = open("euler22.txt", "r")
 x = x.read()
 x = x.split('","')
 x[0] = x[0][1:]
-print x
 x[-1] = x[-1][:-1]
 x = sorted(x)
 
-pv = {}
-s = ord("A")
-for i in range(0,26):
-    pv[chr(s+i)] = i + 1
-
-sum = 0
-for i in x:
-    for a in i:
-        sum += (x.index(i) + 1) * pv[a]
-print sum
+l = {chr(64+i):i for i in range(1,27)}
+print sum((x.index(i) + 1) * l[a] for i in x for a in i)
--- Euler41-50/euler46.py
+++ Euler41-50/euler46.py
@@ -1,12 +1,9 @@
 from primesieve import *
-p = sieve(10000)
-s = [x**2 for x in range(1001) if x **2 < 100000]
-n = (i for i in range(3,100000,2))
-# for i in p:
-#     for a in s:
-#         l.append(i+2*a)
-l = set(i + 2 * a for i in p for a in s)
-for i in n :
-    if i not in l:
+p = sieve(10000) #list of many primes up to arbitrary point
+s = [x**2 for x in range(1001) if x **2 < 100000] #ditto
+n = range(3,100000,2) #must be ordered, have to find FIRST
+l = set(i + 2 * a for i in p for a in s) #not necessary
+for i in n : 
+    if i not in l: #if a number in the range isn't in the special list
         print i
         break
