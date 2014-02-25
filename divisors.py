671fd53b26217372b1d75b638a61220d9ffcb90d
Ajay Dheeraj <obliviator2718@gmail.com>
2014-02-24 19:07:07 -0500

    divisors function

---
+++ divisors.py
@@ -0,0 +1,9 @@
+def divisors2(x):
+    s = set()
+    for i in range(1,int(x**0.5)+1):
+        if not x % i:
+            s.add(i)
+            s.add(x//i)
+    return sorted(list(s))
+print divisors2(1000000000000000000)
+
