1ec21714ca4217c6cdf8148e462e4a5072e8c6fc
Ajay Dheeraj <obliviator2718@gmail.com>
2014-04-11 19:20:58 -0400

    new solution for this

--- Euler31-40/euler34.py
+++ Euler31-40/euler34.py
@@ -1,18 +1,8 @@
 import math
 
-def getdigits(i):
-    return [int(x) for x in str(i)]
+fact = [math.factorial(i) for i in range(10)]
 
-print sum(i for i in xrange(3,1000000) if sum(map(math.factorial,getdigits(i))) == i)
+def getsum(i):
+    return sum(fact[int(x)] for x in str(i))
 
-# def factorial(n):
-#     if n == 0 or n == 1:
-#         return 1
-#     else:
-#         return n * factorial(n-1)
-
-# count = 0
-# for i in range(3,9999999):
-#     if sum(map(factorial,getdigits(i))) == i:
-#         count +=i
-#         print count
+print sum(i for i in xrange(1000000) if getsum(i) == i)
